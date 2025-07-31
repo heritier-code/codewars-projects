103cee605a5bb4963de35d21962d84e64947ed84
heritier-code <mwisenezaheritier4@gmail.com>
2025-07-31 02:09:44 +0200

    initial comit

--- Find the Smallest Integer in the Array.js
+++
@@ -1,7 +0,0 @@
-// Given an array of integers, find and return the smallest integer.
-
-function findSmallestInt(args) {
-  return Math.min(...args);
-}
-
-console.log(findSmallestInt([34, 15, 88, 2]));
